6d8dfabf0b3e4fdd4497724d2ae135e4ad009683
lkz98 <100889027+lkz98@users.noreply.github.com>
2022-04-01 20:14:29 +0800

    程序主函数更新

---
+++ Tri-Func-Calculator-CQU/main.py
@@ -0,0 +1,13 @@
+##########
+# 项目名称：Tri-Func-Calculator-CQU
+# 最终版本：Tri-Func-Calculator-CQU
+# 修订时间：2022年4月1日
+# 小组成员：林康志、杜建建、钟豪、朱思宁、戚俊
+# 程序说明: 定义主函数。
+##########
+
+from calculator import *
+
+if __name__ == '__main__':
+    calc = Calculator()
+    calc.screen.mainloop()
